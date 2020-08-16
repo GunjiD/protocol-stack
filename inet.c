1f15274662562196cda98e237b482827468f8305
Gunzi <29294545a@gmail.com>
2020-08-16 20:25:35 +0900

    fix inet.c

--- inet.c
+++ inet.c
@@ -48,7 +48,7 @@ uint32_t htonl(uint32_t host_long){
     return tmp;    
 }
 
-void ip_ntop(u_int32_t src, char *dest){
+void ntop(u_int32_t src, char *dest){
     u_char addr[16];
     sprintf(addr,"%u.%u.%u.%u",(src >>  0) & 0xFF,(src >>  8) & 0xFF,(src >>  16) & 0xFF,(src >>  24) & 0xFF);
     strcpy(dest, addr);    
