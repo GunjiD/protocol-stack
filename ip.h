829bc0f447a0f533e3908248c6a38634b1fc485b
Gunzi <29294545a@gmail.com>
2020-08-16 19:35:20 +0900

    fix struct ip_hdr

--- ip.h
+++ ip.h
@@ -9,7 +9,7 @@
 
 #include "ethernet.h"
 
-#define IP_LEN 4
+#define IP_STR_LEN 16
 
 typedef struct ip_hdr
 {
@@ -18,8 +18,7 @@ typedef struct ip_hdr
     uint8_t type_of_service;
     uint16_t total_length;
     uint16_t identification;
-    uint16_t flags:3;
-    uint16_t fragment_offset:13;
+    uint16_t fragment_offset;
     uint8_t ttl;
     uint8_t protocol;
     uint16_t hdr_checksum;
