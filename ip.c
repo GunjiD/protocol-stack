0d6a8cbaa720b7329f905260b96df7ad36726a31
Gunzi <29294545a@gmail.com>
2020-10-04 23:08:25 +0900

    Fix ip_hdr_dbg() of the function to clearly understandable.

--- ip.c
+++ ip.c
@@ -14,17 +14,17 @@ void ip_hdr_dbg(struct ip_hdr *ip_header, int dump_form){
 
 switch (dump_form){
         case INTEGER:
-                ntop(ip_header->source_address, ip_src);
-                ntop(ip_header->destination_address, ip_dest);
+                ntop(ntohl(ip_header->source_address), ip_src);
+                ntop(ntohl(ip_header->destination_address), ip_dest);
                 printf("IP Header\n");
-                printf("Version        : %d\n", ip_header->version);
-                printf("IHL            : %d Bytes\n", (ip_header->ihl)*4);
+                printf("Version        : %d\n", ip_header->ihl);
+                printf("IHL            : %d Bytes\n", (ip_header->version)*4);
                 printf("Type_of_Service: %d \n", ip_header->type_of_service);
                 printf("Total Length   : %d Bytes\n", ntohs(ip_header->total_length));                printf("Identification : %2x \n", ip_header->identification);
-                printf("Flagment_Offset: %d \n", ip_header->fragment_offset);
+                printf("Flagment_Offset: %d \n", ntohs(ip_header->fragment_offset));
                 printf("Time To Live   : %d \n", ip_header->ttl);      
                 printf("Protocol       : %d \n", ip_header->protocol);
-                printf("Checksum       : %d \n", ip_header->hdr_checksum);
+                printf("Checksum       : %d \n", ntohs(ip_header->hdr_checksum));
                 printf("source IP      : %s\n", ip_src);
                 printf("destination IP : %s\n", ip_dest);
                 printf("==================================================\n");
@@ -38,17 +38,17 @@ switch (dump_form){
                 for (int i = 0; i < sizeof(*ip_header); ++i) printf("%02X ", hdr[i]);
                 printf("\n");
                 printf("IP Header\n");
-                printf("Version        : %x\n", ip_header->version);
-                printf("IHL            : %x Bytes\n", ip_header->ihl);
+                printf("Version        : %x\n", ip_header->ihl);
+                printf("IHL            : %x Bytes\n", ip_header->version);
                 printf("Type_of_Service: %x \n", ip_header->type_of_service);
-                printf("Total Length   : %2x Bytes\n", ip_header->total_length);
-                printf("Identification : %2x \n", ip_header->identification);
-                printf("Flagment_Offset: %2x \n", ip_header->fragment_offset);
+                printf("Total Length   : %2x Bytes\n", ntohs(ip_header->total_length));
+                printf("Identification : %2x \n", ntohs(ip_header->identification));
+                printf("Flagment_Offset: %2x \n", ntohs(ip_header->fragment_offset));
                 printf("Time To Live   : %x \n", ip_header->ttl);
                 printf("Protocol       : %x \n", ip_header->protocol);
-                printf("Checksum       : %2x \n", ip_header->hdr_checksum);
-                printf("source IP      : %4x \n", ip_header->source_address);
-                printf("destination IP : %4x \n", ip_header->destination_address);
+                printf("Checksum       : %2x \n", ntohs(ip_header->hdr_checksum));
+                printf("source IP      : %4x \n", ntohl(ip_header->source_address));
+                printf("destination IP : %4x \n", ntohl(ip_header->destination_address));
                 printf("__________________________________________________\n");
 
         break;
